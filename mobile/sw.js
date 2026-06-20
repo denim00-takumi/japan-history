37d166331e9c36170c7e5a472a7abe3b22b29b0d
denim00-takumi <denim00@gmail.com>
2026-06-20 13:52:23 +0900

    add mobile PWA

---
+++ mobile/sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'jhm-v1';
+const ASSETS = [
+  '/japan-history/mobile/',
+  '/japan-history/mobile/index.html',
+  '/japan-history/japan_history_data.json',
+  '/japan-history/japan.topojson'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(res => {
+        if (res.ok) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => cached);
+    })
+  );
+});
